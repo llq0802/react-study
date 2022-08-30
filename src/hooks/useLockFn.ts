4c07cc57dedc8db395e5b6482f8502f17cef89b1
李岚清 <958614130@qq.com>
2022-08-30 23:16:59 +0800

    fix: 新增自定义hooks

---
+++ src/hooks/useLockFn.ts
@@ -0,0 +1,24 @@
+import { useRef, useCallback } from 'react';
+
+/**
+ * 用于给一个异步函数增加竞态锁，防止并发执行。
+ */
+export function useLockFn<P extends any[] = any[], V extends any = any>(fn: (...args: P) => Promise<V>) {
+  const lockRef = useRef(false);
+
+  return useCallback(
+    async (...args: P) => {
+      if (lockRef.current) return;
+      lockRef.current = true;
+      try {
+        const ret = await fn(...args);
+        lockRef.current = false;
+        return ret;
+      } catch (e) {
+        lockRef.current = false;
+        throw e;
+      }
+    },
+    [fn]
+  );
+}
