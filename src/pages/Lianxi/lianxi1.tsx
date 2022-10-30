d665c996e8ef33488b695d8d224480099f8dbbb2
李岚清 <958614130@qq.com>
2022-10-30 15:25:32 +0800

    Form组件使用

--- src/pages/Lianxi/lianxi1.tsx
+++ src/pages/Lianxi/lianxi1.tsx
@@ -2,15 +2,10 @@ import React, { useEffect } from 'react';
 import { Form, Input, InputNumber, Typography, Button } from 'antd';
 
 const Demo = () => {
-  const [form] = Form.useForm<{ name: string; age: number }>();
+  const [form] = Form.useForm();
+  const addressValue = Form.useWatch('address', form);
 
-  const nameValue = Form.useWatch('name', form);
-
-  console.log('组件更新', nameValue);
-
-  useEffect(() => {
-    console.log('useEffect', nameValue);
-  }, []);
+  console.log('Form组件更新了');
 
   const getForm = () => {
     // const fields = form.getFieldsValue();
@@ -19,11 +14,12 @@ const Demo = () => {
 
   const setForm = () => {
     form.setFieldsValue({
-      name: '李岚清',
-      age: 25,
+      // name: '李岚清',
+      // age: 25,
+      password: 123,
+      // confirmpassword: 999,
     });
     const fields = form.getFieldsValue();
-
     console.log('fields', fields);
   };
 
@@ -44,29 +40,25 @@ const Demo = () => {
         onFieldsChange={onFieldsChange}
         onValuesChange={onValuesChange}
       >
-        <Form.Item name="name" label="Name (Watch to trigger rerender)">
+        <Form.Item name="name" label="Name (shouldUpdate)">
           <Input />
         </Form.Item>
 
-        <Form.Item name="age" label="Age (Not Watch)">
-          <InputNumber />
+        <Form.Item name="age" label="Age (dependencies)">
+          <Input type="number" />
         </Form.Item>
 
-        {nameValue === '99' ? (
-          <Form.Item name="id" label="ID (Not Watch)">
-            <Input />
-          </Form.Item>
-        ) : null}
+        <Form.Item name="address" label="Address (Use Watch)">
+          <Input />
+        </Form.Item>
 
         <Form.Item name="password" label="Password">
           <Input />
         </Form.Item>
 
         <Form.Item
-          name="text"
-          label="Confirm Password"
-          dependencies={['password']}
-          hasFeedback
+          name="confirmpassword"
+          label="ConfirmPassword (自定义FormItem)"
           rules={[
             {
               required: true,
@@ -82,32 +74,37 @@ const Demo = () => {
             }),
           ]}
         >
-          <Dependencies></Dependencies>
+          <Component />
         </Form.Item>
 
-        {/* <Form.Item>
-          <div></div>
-        </Form.Item> */}
+        <Form.Item dependencies={['age']}>
+          {(formInstance) => {
+            console.log('dependencies', formInstance);
+            return 'dependencies';
+          }}
+        </Form.Item>
+
+        <Form.Item shouldUpdate={(prevValues, curValues) => prevValues.name !== curValues.name}>
+          {(formInstance) => {
+            console.log('shouldUpdate', formInstance);
+            return 'shouldUpdate';
+          }}
+        </Form.Item>
       </Form>
 
       <Typography>
-        <pre>Name Value: {nameValue}</pre>
+        <pre>addressValue Value: {addressValue}</pre>
       </Typography>
-
-      <Typography>
-        <pre>Name AGE: {form.getFieldValue('age')}</pre>
-      </Typography>
-
-      <Button onClick={getForm}>获取</Button>
 
       <Button onClick={setForm}>更新</Button>
     </>
   );
 };
 
-export default Demo;
-
-const Dependencies = (props) => {
-  console.log('Dependencies-props ', props);
-  return <Input />;
+const Component = (props) => {
+  const { value, onChange } = props;
+  console.log('自定义FormItem ');
+  return <Input value={value} onChange={onChange} defaultValue={value} />;
 };
+
+export default Demo;
